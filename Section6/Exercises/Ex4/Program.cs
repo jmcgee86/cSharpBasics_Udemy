1ad4d27c6048c7feeb0508ae0079d1ef32592069
Jim McGee <jmcgee86@gmail.com>
2018-05-01 18:35:32 -0400

    section6 complete

---
+++ Section6/Exercises/Ex4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex4_Section6
+{
+    class Program
+    {
+        //Write a program and ask the user to continuously enter a number or type "Quit" to exit. 
+        //The list of numbers may include duplicates. Display the unique numbers that the user has entered.
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            var input = "continue";
+            var uniqueNums = new List<int>();
+            while(true)
+            {
+                
+                System.Console.Write("Please enter a number or quit to exit: ");
+                input = Console.ReadLine();
+                if (input == "Quit")
+                    break;
+                else
+                {
+                    var numInput = int.Parse(input);
+                    bool found = false;
+                    for(var i = 0; i<numbers.Count; i++)
+                    {
+                        if (numInput == numbers[i])
+                        {
+                            found = true;
+                            break;
+                        }
+                    }  
+                    numbers.Add(numInput);
+
+                    if (found == false)
+                    {
+                        uniqueNums.Add(numInput);
+                    }   
+                    else
+                    {
+                        found = false;
+                    }
+            }
+        }
+            foreach (var n in uniqueNums)
+            System.Console.WriteLine(n);
+        }
+    }
+
+}
---
+++ Section6/Exercises/Ex5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Ex5_Section6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray;
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a list of comma-separated numbers: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    inputArray = input.Split(',');
+                    if (inputArray.Length >= 5)
+                        break;
+                }
+
+                Console.WriteLine("Invalid List");
+            }
+
+            foreach (var n in inputArray){
+                numbers.Add(int.Parse(n));
+            }
+            numbers.Sort();
+
+            for (var i = 0; i<3; i++)
+            System.Console.WriteLine(numbers[i]);
+
+        }
+    }
+}
