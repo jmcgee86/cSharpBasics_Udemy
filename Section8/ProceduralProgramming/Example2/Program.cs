949519b1151c33d88674fdd20cced3c1878933f3
Jim McGee <jmcgee86@gmail.com>
2018-05-03 18:24:29 -0400

    Section 8 complete

---
+++ Section8/ProceduralProgramming/Example2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var numbers = new List<int>();
+
+           while (true)
+           {
+               System.Console.WriteLine("Enter a number (or 'Quit to exit): ");
+               var input = Console.ReadLine();
+               if (input.ToLower()=="quit")
+                break;
+
+                numbers.Add(Convert.ToInt32(input));
+
+                var uniques = GetUniqueNumbers(numbers);
+                
+                System.Console.WriteLine("Unique numbers: ");
+                foreach(var number in uniques)
+                    System.Console.WriteLine(number);
+           }
+        }
+
+        public static List <int> GetUniqueNumbers (List<int> numbers)
+        {
+            var uniques = new List <int>();
+                foreach(var number in numbers)
+                {
+                    if (!uniques.Contains(number))
+                        uniques.Add(number);
+                }
+                return uniques;
+        }
+    }
+}
---
+++ Section8/ProceduralProgramming/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProceduralProgramming_Section8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+
+            var reversed = ReversedName(name);
+
+            System.Console.WriteLine("Reversed name: " + reversed);
+        }
+
+        public static string ReversedName (string name)
+        {
+            var array = new char[ name.Length ];
+            for(var i = name.Length; i > 0; i--)
+                array[ name.Length - i ] = name[ i - 1 ];
+            
+            //var reversed = new string (array);
+            //return reversed;
+            return new string (array);
+        }
+    }
+}
